5ee9552a7fca3d0bb2ac20ea4dce71ba98a3c61d
Paulo Freire <paulo.freiire@hotmail.com>
2024-06-18 14:36:04 -0300

    initial commit

---
+++ src/components/Brand.vue
@@ -0,0 +1,10 @@
+<script setup>
+</script>
+
+<template>
+	<h1 class="navbar-brand navbar-brand-autodark">
+	                <a href=".">
+	                    <img src="../../dist/static/logo.svg" width="110" height="32" alt="Tabler" class="navbar-brand-image">
+	                </a>
+	</h1>
+</template>
---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,14 @@
+<script setup>
+  import '../../dist/js/demo-theme.js';
+  import '../../dist/js/tabler.min.js';
+  import '../../dist/js/demo.min.js';
+  import SideBar from './SideBar.vue';
+  import NavBar from './NavBar.vue';
+</script>
+
+<template>
+<div class="page">
+  <NavBar />
+  <SideBar />
+</div>
+</template>
